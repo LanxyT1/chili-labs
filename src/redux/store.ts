feb9658e21d9a0d3b964f0e77ae1c494a87c1bbd
Ruslans <ruslans.rezanovs@mapon.com>
2025-12-11 14:55:29 +0200

    Added redux toolkit support

---
+++ src/redux/store.ts
@@ -0,0 +1,13 @@
+/*Copyright © 2025 Chili Labs. All rights reserved.*/
+
+import { configureStore } from "@reduxjs/toolkit";
+import phonesReducer from "./slices/phonesSlice";
+
+export const store = configureStore({
+    reducer: {
+        phones: phonesReducer,
+    },
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
